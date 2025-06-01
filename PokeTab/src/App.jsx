8ad0ea0792180019d9c603d83f50b00089a93ff1
Arnav Sirohi <arnavsirohi007@gmail.com>
2025-06-01 15:34:25 +0530

    Final commit

--- PokeTab/src/App.jsx
+++ PokeTab/src/App.jsx
@@ -3,12 +3,12 @@ import Main from "./LandingPage/Main";
 
 function App() {
   return (
-    <div >
-
+    <div className="container text-center mt-5">
+      <h1 className="display-3 fw-bold text-danger heading-shadow">
+        POKÉMON APP
+      </h1>
       <Main />
     </div>
-
-
   );
 }
 
